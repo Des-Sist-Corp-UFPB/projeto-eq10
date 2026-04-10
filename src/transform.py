e22f8585e7583e98155770285e53fbcd4bf55db0
Gabriel-Nunes-UFPB <gabriel.nunes@dcx.ufpb.br>
2026-04-10 16:19:31 -0300

    Criada classe dic.py
    Criada def transform_rename_columns

--- src/transform.py
+++ src/transform.py
@@ -1,10 +1,15 @@
 import pandas as pd
 from pathlib import Path
 import pyarrow
+from dic import dic_rename_columns
 
 
 
 def transform_remove_columns(file_path) -> pd.DataFrame:
     df = pd.read_parquet(file_path, engine="pyarrow", columns=["PA_QTDAPR", "PA_QTDPRO", "PA_VALAPR", "PA_VALPRO", "PA_UFMUN", "PA_MUNPCN", 
                                                                             "PA_MVM", "PA_RACACOR", "PA_IDADE", "PA_CODUNI", "PA_CBOCOD", "PA_PROC_ID"])
+    return df
+
+def transform_rename_columns(df) -> pd.DataFrame:
+    df = df.rename(columns=dic_rename_columns)
     return df
