a5f9c48177c6169853a7c2253b9e417d1836b3e3
Gabriel-Nunes-UFPB <gabriel.nunes@dcx.ufpb.br>
2026-04-10 16:35:29 -0300

    testando transform_rename_columns

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pysus.online_data.SIA import SIA
 import pandas as pd
 from datetime import datetime
 from src.extract import extract_data
-from src.transform import transform_remove_columns
+from src.transform import transform_remove_columns, transform_rename_columns
 from pathlib import Path
 
 sia = SIA().load()
@@ -15,6 +15,7 @@ file_path = BASE_DIR / "data" / "sia_datasus.parquet"
 if __name__ == "__main__":
     extract_data(sia)
     df = transform_remove_columns(file_path)
+    df= transform_rename_columns(df)
     print(df.head())
 
 
--- src/transform.py
+++ src/transform.py
@@ -1,9 +1,21 @@
 import pandas as pd
 from pathlib import Path
 import pyarrow
-from dic import dic_rename_columns
 
-
+dic_rename_columns = {
+    "PA_QTDAPR" : "frequencia",
+    "PA_QTDPRO" : "quantidade_apresentada",
+    "PA_VALAPR" : "valor_aprovado",
+    "PA_VALPRO" : "valor_apresentado",
+    "PA_UFMUN" : "municipio_atendido",
+    "PA_MUNPCN" : "municipio_residencia",
+    "PA_MVM" : "data",
+    "PA_RACACOR" : "raca_cor",
+    "PA_IDADE" : "idade",
+    "PA_CODUNI" : "cnes",
+    "PA_CBOCOD" : "codigo_brasileiro_ocupacao",
+    "PA_PROC_ID" : "codigo_procedimento"
+}
 
 def transform_remove_columns(file_path) -> pd.DataFrame:
     df = pd.read_parquet(file_path, engine="pyarrow", columns=["PA_QTDAPR", "PA_QTDPRO", "PA_VALAPR", "PA_VALPRO", "PA_UFMUN", "PA_MUNPCN", 
@@ -13,3 +25,4 @@ def transform_remove_columns(file_path) -> pd.DataFrame:
 def transform_rename_columns(df) -> pd.DataFrame:
     df = df.rename(columns=dic_rename_columns)
     return df
+
