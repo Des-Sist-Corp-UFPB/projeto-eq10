52b19c6288b5ea4d28fc65897a9b070d7635e099
Gabriel-Nunes-UFPB <gabriel.nunes@dcx.ufpb.br>
2026-04-13 14:33:32 -0300

    criada e testada def transform_filter_units

--- src/transform.py
+++ src/transform.py
@@ -4,6 +4,8 @@ import pyarrow
 
 lista_filtros_cidades = ["250150", "251250", "250890"] # "250150": "Bananeiras"/ "251250": "Queimadas"/ "250890": "Mamanguape",
 
+list_units = ["2597349", "7176295", "7654553", "5427207", "5489822", "3769593", "0840386", "2932709", "5906822", "8095973", "8077576", "8077584", "2871467", "7173814", "6389082", "0617342"]
+
 dic_rename_columns = {
     "PA_QTDAPR" : "frequencia",
     "PA_QTDPRO" : "quantidade_apresentada",
@@ -30,7 +32,10 @@ def transform_rename_columns(df) -> pd.DataFrame:
 
 def transform_filter_city(df) -> pd.DataFrame:
     df = df[df["PA_UFMUN"].isin(lista_filtros_cidades)]
-    if(len(lista_filtros_cidades) == df["PA_UFMUN"].unique().size):
+    if(len(lista_filtros_cidades) == len(df["PA_UFMUN"].unique())):
         return df
     
-
+def transform_filter_units(df) -> pd.DataFrame:
+    df = df[df["PA_CODUNI"].isin(list_units)]
+    if(len(list_units) == len(df["PA_CODUNI"].unique())):
+        return df
