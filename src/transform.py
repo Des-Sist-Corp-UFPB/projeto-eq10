25402c818ca77f4cb6094485602af8d73d1c0401
Riansito <rianfreires40@gmail.com>
2026-04-13 15:44:29 -0300

    refactor: ajusta nomes das colunas nos filtros para usar padrão definido após renomeação

--- src/transform.py
+++ src/transform.py
@@ -2,9 +2,16 @@ import pandas as pd
 from pathlib import Path
 import pyarrow
 
-lista_filtros_cidades = ["250150", "251250", "250890"] # "250150": "Bananeiras"/ "251250": "Queimadas"/ "250890": "Mamanguape",
+list_filter_cities = ["250150", "251250", "250890"] # "250150": "Bananeiras"/ "251250": "Queimadas"/ "250890": "Mamanguape",
 
-list_units = ["2597349", "7176295", "7654553", "5427207", "5489822", "3769593", "0840386", "2932709", "5906822", "8095973", "8077576", "8077584", "2871467", "7173814", "6389082", "0617342"]
+list_units = ["2597349", "7176295", "7654553", "5427207", "5489822", "3769593", 
+              "0840386", "2932709", "5906822", "8095973", "8077576", "8077584", 
+              "2871467", "7173814", "6389082", "0617342", "3941108", "091023", 
+              "2418703", "2418711", "4333640", "7157002", "6910327", "5103916",
+              "4787994","2341212","2908247","6199194","7021747","6924611","6859011"]
+
+list_filter_columns = ["PA_QTDAPR", "PA_QTDPRO", "PA_VALAPR", "PA_VALPRO", "PA_UFMUN", "PA_MUNPCN", 
+                        "PA_MVM", "PA_RACACOR", "PA_IDADE", "PA_CODUNI", "PA_CBOCOD", "PA_PROC_ID"]
 
 dic_rename_columns = {
     "PA_QTDAPR" : "frequencia",
@@ -22,8 +29,7 @@ dic_rename_columns = {
 }
 
 def transform_remove_columns(file_path) -> pd.DataFrame:
-    df = pd.read_parquet(file_path, engine="pyarrow", columns=["PA_QTDAPR", "PA_QTDPRO", "PA_VALAPR", "PA_VALPRO", "PA_UFMUN", "PA_MUNPCN", 
-                                                                            "PA_MVM", "PA_RACACOR", "PA_IDADE", "PA_CODUNI", "PA_CBOCOD", "PA_PROC_ID"])
+    df = pd.read_parquet(file_path, engine="pyarrow", columns=list_filter_columns)
     return df
 
 def transform_rename_columns(df) -> pd.DataFrame:
@@ -31,11 +37,13 @@ def transform_rename_columns(df) -> pd.DataFrame:
     return df
 
 def transform_filter_city(df) -> pd.DataFrame:
-    df = df[df["PA_UFMUN"].isin(lista_filtros_cidades)]
-    if(len(lista_filtros_cidades) == len(df["PA_UFMUN"].unique())):
+    df = df[df["municipio_atendido"].isin(list_filter_cities)]
+    if(len(list_filter_cities) == len(df["municipio_atendido"].unique())):
         return df
     
 def transform_filter_units(df) -> pd.DataFrame:
-    df = df[df["PA_CODUNI"].isin(list_units)]
-    if(len(list_units) == len(df["PA_CODUNI"].unique())):
+    df = df[df["cnes"].isin(list_units)]
+    if(len(df["cnes"].unique()) <= len(list_units)):
         return df
+
+
