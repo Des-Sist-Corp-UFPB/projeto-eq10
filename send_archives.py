e8447dc8f50e8cc080a04d99c9e4e6d435cbf40b
Gabriel-Nunes-UFPB <gabriel.nunes@dcx.ufpb.br>
2026-04-17 08:24:53 -0300

    feat: adiciona carga das dimensões de raça/cor e município ao pipeline de ETL

--- send_archives.py
+++ send_archives.py
@@ -14,10 +14,16 @@ file_path = BASE_DIR / "data" / "sia_datasus.parquet"
 file_path_to_save = BASE_DIR / "data"/ "sia_datasus_transformed.parquet"
 file_path_to_save_ocupacao = BASE_DIR / "data"/ "dim_ocupacao.parquet"
 file_path_to_save_procedimentos = BASE_DIR / "data"/ "dim_procedimentos.parquet"
+file_path_to_save_raca_cor = BASE_DIR / "data"/ "dim_raca_cor.parquet"
+file_path_to_save_municipio = BASE_DIR / "data"/ "dim_municipio.parquet"
 
 if __name__ == "__main__":
     df_ocupacao = pd.read_parquet(file_path_to_save_ocupacao, engine = "pyarrow")
     df_procedimentos = pd.read_parquet(file_path_to_save_procedimentos, engine = "pyarrow")
+    df_raca_cor = pd.read_parquet(file_path_to_save_raca_cor, engine = "pyarrow")
+    df_municipios = pd.read_parquet(file_path_to_save_municipio, engine = "pyarrow")
     load_data_sus('dim_procedimento', df_procedimentos)
     load_data_sus('dim_ocupacao', df_ocupacao)
+    load_data_sus('dim_raca_cor', df_raca_cor)
+    load_data_sus('dim_municipio', df_municipios)
     print("ETL feito com sucesso!")
